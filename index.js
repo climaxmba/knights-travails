099949231a47746089b33646cd1e7f5cb057800a
Climax Mba <climaxium150@gmail.com>
2023-09-28 13:44:35 +0100

    Add ChessBoard and Vertex

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
+class Vertex {
+  constructor(value) {
+    this.value = value;
+    this.adjVertices = [];
+  }
+
+  addAdjVertex(...vertices) {
+    vertices.forEach(vertex => {
+      if (vertex) this.adjVertices.push(vertex);
+    })
+  }
+}
+
 class Graph {
   constructor() {
     this.vertices = {};
   }
 
-  addVertex(vertex) {
-    this.vertices[vertex] = vertex;
+  addVertex(value) {
+    this.vertices[value] = new Vertex(value);
   }
 
   addEdge(source, destination) {
-    this.vertices[source].push(destination);
+    this.vertices[source].adjVertices.push(this.vertices[destination]);
   }
 
   getAdj(vertex) {
-    return this.vertices[vertex];
+    return this.vertices[vertex].adjVertices;
   }
 }
 
-const chessBoard = new Graph();
+class ChessBoard extends Graph {
+  constructor(length = 8) {
+    super();
+    this.#initBoard(length);
+  }
+
+  #initBoard(length) {
+    // Add verticies
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length; j++) {
+        this.addVertex(`${i}, ${j}`);
+      }
+    }
+
+    // Add edges
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length; j++) {
+        this.vertices[`${i}, ${j}`].addAdjVertex(
+          this.vertices[`${i + 2}, ${j + 1}`],
+          this.vertices[`${i + 2}, ${j - 1}`],
+          this.vertices[`${i - 2}, ${j + 1}`],
+          this.vertices[`${i - 2}, ${j - 1}`],
+          this.vertices[`${i + 1}, ${j + 2}`],
+          this.vertices[`${i + 1}, ${j - 2}`],
+          this.vertices[`${i - 1}, ${j + 2}`],
+          this.vertices[`${i - 1}, ${j - 2}`]
+        );
+      }
+    }
+  }
+}
+
+const chessBoard = new ChessBoard();
+console.log(chessBoard);
