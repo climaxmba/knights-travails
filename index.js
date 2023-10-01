4d9afc439ad0b24c5f30203e94e9063c466afede
Climax Mba <climaxium150@gmail.com>
2023-10-01 14:59:31 +0100

    Complete knightMoves function

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const ChessBoard = require("./ChessBoard");
 
-(function knightMoves(from = [3, 3], to = [0, 5]) {
-  const chessBoard = new ChessBoard();
-  console.log(chessBoard.shortestKnightPath(from.toString(), to.toString()));
-})();
+function knightMoves(from = [3, 3], to = [0, 5]) {
+  const path = new ChessBoard().shortestKnightPath(
+    from.toString(),
+    to.toString()
+  );
+  
+  console.log(
+    `You made it in ${
+      path.length === 1 ? path.length + " move" : path.length + " moves"
+    }!  Here's your path:`
+  );
+  path.forEach((coord) => console.log(coord));
+}
+
+// Tests
+knightMoves([0, 0], [1, 2]);
+knightMoves([0, 0], [3, 3]);
+knightMoves([3, 3], [0, 0]);
